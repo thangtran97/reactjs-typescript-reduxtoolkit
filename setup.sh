4fc929e885d422bb31265197ef6298509889c27c
tuanha <tuanha@nissho-vn.com>
2022-10-03 11:47:15 +0700

    update setup shell

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 pip install -r ./backend/requirements.txt && \
 yarn --cwd backend-nodejs install && \
-yarn --cwd frontend/learning-typescript install
+yarn --cwd frontend/learning-typescript install && \
+sh -c 'export NVRPATH="$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$1")" >> /etc/profile.d/nvr-env' && \
+cd RTSPtoWeb_server/; go build
 
