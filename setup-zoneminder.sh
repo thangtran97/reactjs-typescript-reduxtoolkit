13595da2f32cd33cec25fa9aacab90f1c996862e
tuanha <tuanha@nissho-vn.com>
2022-10-14 15:11:41 +0700

    update script init setup zoneminder

--- setup-zoneminder.sh
+++ setup-zoneminder.sh
@@ -1,9 +1,9 @@
 apt update -y; \
 apt install apache2 mysql-server php -y; \
 systemctl enable --now mysql && \
-add-apt-repository ppa:iconnor/zoneminder-1.36; \
+add-apt-repository --yes ppa:iconnor/zoneminder-1.36; \
 apt update -y && apt upgrade -y; \
-apt install zoneminder; \
+apt install zoneminder -y; \
 rm /etc/mysql/my.cnf; \
 cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/my.cnf; \
 sh -c 'echo "sql_mode = NO_ENGINE_SUBSTITUTION" >> /etc/mysql/my.cnf'; \
@@ -13,5 +13,5 @@ chown -R www-data:www-data /usr/share/zoneminder/; \
 a2enmod cgi rewrite expires headers; \
 a2enconf zoneminder; \
 service zoneminder start; \
-service apache2 start
+service apache2 reload
 
